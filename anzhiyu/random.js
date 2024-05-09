ea8bb1e0ac4fb193ec6514a1558f837d4234d77c
kele <76410006@qq.com>
2024-05-09 12:23:48 +0800

    Site updated: 2024-05-09 12:23:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/08/用户中心/","2024/05/08/部署上线踩坑/","2024/05/08/马赛克项目部署文档/","2024/05/09/page/"];function toRandomPost(){
+var posts=["2024/05/08/用户中心/","2024/05/08/部署上线踩坑/","2024/05/08/马赛克项目部署文档/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
